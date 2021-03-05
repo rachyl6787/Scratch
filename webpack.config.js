25bbe689696832e2ed02ba189987069a1541d37c
Christopher Bosserman <christopherpbosserman@gmail.com>
2021-03-05 17:04:17 -0500

    goblin skeleton

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  plugins: [new Dotenv()],
+  entry: path.resolve(__dirname, 'client', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  mode: process.env.NODE_ENV,
+  devServer: {
+    publicPath: '/build/',
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /.(css|scss)$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};

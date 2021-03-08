20c8b41a546b93af3eb5f4f2acb66a91ea58a083
Timeo <timeo@Timeos-Mac-mini.local>
2021-03-08 02:21:01 -0500

    Revised schema SQL

--- Festival Playlist Schema_postgres_create.sql
+++ Festival Playlist Schema_postgres_create.sql
@@ -1,50 +1,64 @@
-CREATE TABLE "Users" (
-	"id" bigint NOT NULL,
-	"name" varchar(255) NOT NULL,
-	"artists_id" bigint(255) NOT NULL,
-	"festivals_id" bigint(255) NOT NULL,
-	CONSTRAINT "Users_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
-);
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
 
 
 
-CREATE TABLE "Festivals" (
-	"_id" serial NOT NULL,
-	"name" serial(255) NOT NULL,
-	CONSTRAINT "Festivals_pk" PRIMARY KEY ("_id")
-) WITH (
-  OIDS=FALSE
-);
-
-
-
-CREATE TABLE "Artists" (
+CREATE TABLE public.people (
 	"id" serial NOT NULL,
-	"name" serial(255) NOT NULL,
-	CONSTRAINT "Artists_pk" PRIMARY KEY ("id")
+	"name" varchar NOT NULL,
+	"artists_id" bigint,
+	"festivals_id" bigint,
+	CONSTRAINT "people_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
 
 
 
-CREATE TABLE "Songs" (
+CREATE TABLE public.festivals (
 	"_id" serial NOT NULL,
-	"name" serial(255) NOT NULL,
-	CONSTRAINT "Songs_pk" PRIMARY KEY ("_id")
+	"name" varchar NOT NULL,
+	CONSTRAINT "festivals_pk" PRIMARY KEY ("_id")
 ) WITH (
   OIDS=FALSE
 );
 
 
 
-ALTER TABLE "Users" ADD CONSTRAINT "Users_fk0" FOREIGN KEY ("artists_id") REFERENCES "Artists"("id");
-ALTER TABLE "Users" ADD CONSTRAINT "Users_fk1" FOREIGN KEY ("festivals_id") REFERENCES "Festivals"("_id");
+CREATE TABLE public.artists (
+	"id" serial NOT NULL,
+	"name" varchar NOT NULL,
+	CONSTRAINT "artists_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
 
-ALTER TABLE "Festivals" ADD CONSTRAINT "Festivals_fk0" FOREIGN KEY ("_id") REFERENCES "Artists"("id");
-
-ALTER TABLE "Artists" ADD CONSTRAINT "Artists_fk0" FOREIGN KEY ("id") REFERENCES "Songs"("_id");
 
 
+CREATE TABLE public.songs (
+	"_id" serial NOT NULL,
+	"name" varchar NOT NULL,
+	CONSTRAINT "songs_pk" PRIMARY KEY ("_id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+ALTER TABLE public.people ADD CONSTRAINT "people_fk0" FOREIGN KEY ("artists_id") REFERENCES public.artists("id");
+ALTER TABLE public.people ADD CONSTRAINT "people_fk1" FOREIGN KEY ("festivals_id") REFERENCES public.festivals("_id");
+
+ALTER TABLE public.festivals ADD CONSTRAINT "festivals_fk0" FOREIGN KEY ("_id") REFERENCES public.artists("id");
+
+ALTER TABLE public.artists ADD CONSTRAINT "artists_fk0" FOREIGN KEY ("id") REFERENCES public.songs("_id");
+
+
+-- Insert sample data from SongPick Api
